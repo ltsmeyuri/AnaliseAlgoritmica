1d82f0c53c9148fa9cd1a439ffaa6bb8cc9f98c1
Yuri <112394310+ltsmeyuri@users.noreply.github.com>
2023-11-10 22:51:31 -0300

    Create FibonacciRecursiva.java

---
+++ FibonacciRecursiva.java
@@ -0,0 +1,23 @@
+public class Fibonacci {
+
+    // Versão recursiva
+    public static long fibonacciRecursivo(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacciRecursivo(n - 1) + fibonacciRecursivo(n - 2);
+        }
+    }
+
+    // Outras versões aqui...
+    
+    public static void main(String[] args) {
+        int n = 40;  // Ajuste conforme necessário
+        long startTime = System.nanoTime();
+        long resultado = fibonacciRecursivo(n);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1_000_000; // convertendo para milissegundos
+        System.out.println("Fibonacci Recursivo de " + n + ": " + resultado);
+        System.out.println("Tempo de execução: " + duration + " ms");
+    }
+}
